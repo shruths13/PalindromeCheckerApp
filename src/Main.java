06e1b54a352cce36c5cd2d830871f0f4dfdc2bf4
shruths13 <shruths7@gmail.com>
2026-03-09 13:56:30 +0530

    UC1: Add Welcome Message module

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String input = "madam";
-        boolean isPalindrome = true;
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
 
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome) {
-            System.out.println(input + " is a Palindrome");
-        } else {
-            System.out.println(input + " is not a Palindrome");
-        }
     }
 }
