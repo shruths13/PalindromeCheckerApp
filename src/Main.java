4bf697fa9d24a63a4eaaa3fc1a634e2cb3ceadc9
shruths13 <shruths7@gmail.com>
2026-03-09 15:01:04 +0530

    UC6:Queue + Stack based plaindrome check

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,30 @@
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
-
-public class Main{
-
+public class Main {
     public static void main(String[] args) {
-
-        String input = "noon";
-
+        String input = "civic";
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
-
         for (char c : input.toCharArray()) {
+            queue.add(c);
             stack.push(c);
         }
-
         boolean isPalindrome = true;
 
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+        while (!queue.isEmpty()) {
+
+            char fromQueue = queue.remove();
+            char fromStack = stack.pop();
+
+            if (fromQueue != fromStack) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        if (isPalindrome) {
-            System.out.println(input + " is a Palindrome");
-        } else {
-            System.out.println(input + " is not a Palindrome");
-        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
